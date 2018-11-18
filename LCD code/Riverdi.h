68dd69b0a8a8f3acf6b5ad142a4bd1e2a8390c87
Abdulrahman Tarek Mohammad <44070911+Battawi@users.noreply.github.com>
2018-11-18 21:34:42 +0200

    Update and rename Function_Declaration.h to Riverdi.h

---
+++ LCD code/Riverdi.h
@@ -0,0 +1,24 @@
+/*
+ * Riverdi.h
+ *
+ * Created: 29/10/2018 07:00:51 م
+ *  Author: Alaa
+ */ 
+
+
+#ifndef RIVERDI_H_
+#define RIVERDI_H_
+											/*Functions Declaration*/
+uint8_t FT8_init(void);
+void Riverdi_init(void);
+
+
+											/*Pin declarations*/
+
+
+#define POWER_PIN PA1
+
+
+
+
+#endif /* RIVERDI_H_ */
