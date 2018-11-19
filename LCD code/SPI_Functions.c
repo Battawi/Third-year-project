296c518bcd85c1b3d86686c96cf8c5edd802265c
Rim0o <44038877+Rim0o@users.noreply.github.com>
2018-11-19 23:28:04 +0200

    Update SPI_Functions.c
    Edited the first API only, made some comments and editing the 3rd parameter in spi_write

--- LCD code/SPI_Functions.c
+++ LCD code/SPI_Functions.c
@@ -1,7 +1,11 @@
+#define CS0 0 //define the CS0 to be replaced in code with 0
+
+
+//write 16 bits to Riverdi, start with sending address from MCU to TFT and then send data
 void Riverdi_write_16(uint32_t address,uint16_t data){
-	spi_write(SPI,(uint16_t)(address >> 16) | MEM_WRITE,SPI_CHIP_PCS,0); //lesa htt3del
-	spi_write(SPI,(uint16_t)(address),SPI_CHIP_PCS,0);
-	spi_write(SPI,data,SPI_CHIP_PCS,1);
+	spi_write(SPI, (uint16_t)(address >> 16) | MEM_WRITE, spi_get_pcs(CS0), 0); //need to configure PCS,LASTXFER for fixed mode
+	spi_write(SPI, (uint16_t)(address), spi_get_pcs(CS0), 0); //as above
+	spi_write(SPI, data, spi_get_pcs(CS0), 1); //as above
 }
 //---------------------------------------------------
 void Riverdi_write_32(uint32_t address,uint32_t data){
